0ff6ba20bec91320e4baeebff4b9c74713e2c508
erikyuzwa <erikyuzwa@gmail.com>
2023-09-30 09:22:12 -0600

    initial project

---
+++ src/main/java/com/example/springboot3graphqldemo/SpringBoot3GraphqlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot3graphqldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3GraphqlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3GraphqlDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot3graphqldemo/SpringBoot3GraphqlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot3graphqldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3GraphqlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
